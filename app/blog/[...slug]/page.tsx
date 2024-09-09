04778fe7d6702378accf5ca51dc87f3bafcece8e
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2024-09-09 13:08:33 +0800

    feat: update blog page layout & toc

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,6 +1,6 @@
-import { MarkdownTOC } from '@/components/MarkdownToc/page';
 import { allDocuments } from 'contentlayer/generated';
 import { format } from 'date-fns';
+import { TOC } from 'react-markdown-toc/server';
 
 export default function Page({ params }: Readonly<{ params: { slug: string[] } }>) {
     const currentDocument = allDocuments.find((doc) => doc.slug === params.slug[params.slug.length - 1]);
@@ -9,18 +9,24 @@ export default function Page({ params }: Readonly<{ params: { slug: string[] } }
         return <div>Document not found</div>;
     }
 
+    console.log(currentDocument.body.html);
+
     return (
         <main className='mx-auto min-h-[calc(100vh-10rem)] w-[60vw]'>
-            <p className='text-sm text-zinc-400'>{currentDocument?.summary}</p>
-
-            <MarkdownTOC markdownString={currentDocument?.body.raw ?? ''} />
-
-            <div className='my-8 text-lg text-zinc-400'>
+            <div className='text-lg text-zinc-400'>
                 <span>{currentDocument?.tags.join(' ').toUpperCase()}</span>
                 <span className='float-right'>{format(new Date(currentDocument?.date), 'yyyy-MM-dd')}</span>
             </div>
 
-            <article className='prose prose-base mx-auto min-w-[60vw]' dangerouslySetInnerHTML={{ __html: currentDocument?.body.html ?? '' }} />
+            <p className='my-8 text-sm text-zinc-400'>{currentDocument?.summary}</p>
+
+            <div className='relative flex justify-between gap-16'>
+                <article className='prose prose-base min-w-[40vw]' dangerouslySetInnerHTML={{ __html: currentDocument?.body.html ?? '' }} />
+
+                <div className='sticky top-16 h-fit'>
+                    <TOC markdown={currentDocument?.body.raw ?? ''} className='ml-4 text-zinc-400' ul='pl-4' />
+                </div>
+            </div>
         </main>
     );
 }
